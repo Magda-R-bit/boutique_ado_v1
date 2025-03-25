0fffdacf123f158d85c7ae22d702f8b33a53af02
Magda <magda.redka@gmail.com>
2025-03-25 12:23:52 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key':'pk_test_51R6Vq0IwVXTmqLKKpm7CfichUFCtg0AcNz0vFOnB6898ClOVwpl70OWZURDSoJtUmqAE7WDhpFB5wYn8WUaYSl1600XMbXgQ4a',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
